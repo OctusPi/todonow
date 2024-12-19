19fcb2590109a77d8920684af3e85bd3ab009364
octuspi <octuspi@gmail.com>
2024-12-19 09:32:59 -0300

    feat: add backdrop blur to modals

--- frontend/src/views/TasksView.vue
+++ frontend/src/views/TasksView.vue
@@ -4,7 +4,7 @@
     <Dialog as="div" class="relative z-10">
       <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
         leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
-        <div class="fixed inset-0 bg-black/25" />
+        <div class="fixed inset-0 bg-black/25 backdrop-blur-md" />
       </TransitionChild>
 
       <div class="fixed inset-0 overflow-y-auto">
